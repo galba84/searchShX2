5b4a7b85eba4fff059d39d02c8895901501ef3a3
Oleksandr Sereda <sered2a@gmail.com>
2021-04-11 15:27:23 +0300

    implementation v 1.0.1.

---
+++ src/test/java/org/example/CalcTest.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalcTest {
+
+    Calc testInstance = new Calc();
+    int testValue = -4;
+
+    @Test
+    void testShReturnCorrectValue() {
+        double result = testInstance.findSh(testValue);
+        double roundedResult = Math.round(result * 10) / 10.0d;
+        double referenceResult = Math.sinh(testValue);
+        double roundedReferenceResult = Math.round(referenceResult * 10) / 10.0d;
+        assertEquals(roundedResult, roundedReferenceResult);
+    }
+
+    @Test
+    void testShResultDiffersInLessThanThirdElement() {
+        double result = testInstance.findSh(testValue);
+        double referenceResult = Math.sinh(testValue);
+        double difference = result - referenceResult;
+        assert (difference < 0.1);
+    }
+
+    @Test
+    void testShResultDiffersFromReferentResult() {
+        double result = testInstance.findSh(testValue);
+        double referenceResult = Math.sinh(testValue);
+        assert (result != referenceResult);
+    }
+}
