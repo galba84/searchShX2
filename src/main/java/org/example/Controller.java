5f092c4b53a4fe2609007f78dcb3b780f7729d27
Oleksandr Sereda <sered2a@gmail.com>
2021-04-11 15:24:42 +0300

    implementation v 1.0.1.

---
+++ src/main/java/org/example/Controller.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    private static final String template = "Sh (%s) is : %f";
+
+    @GetMapping("/findSh")
+    public Responce findSh(@RequestParam(value = "value", defaultValue = "0") String value) {
+        Calc calc = new Calc();
+        double result = calc.findSh(Integer.parseInt(value));
+        return new Responce( String.format(template, value, result));
+    }
+}
---
+++ src/main/java/org/example/Responce.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Responce {
+
+    private final String content;
+
+    public Responce(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
